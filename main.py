c51e03d11dd254fa13049bfe91b18fa0be5dc4c3
xlupicomas <xlupicomas@gmail.com>
2023-10-09 20:35:16 +0200

    ejercicio 1 pythom-pseint

--- main.py
+++ main.py
@@ -1,10 +1,50 @@
+import math
+
+
 print("intorudce A")
-	A = int(input("A"))
-	print("introduce B")
-	B = int(input("B"))
-	print(" A vale ", A," y B vale",B,)
+A = int(input("que numero es A"))
+print("introduce B")
+B = int(input("que numero es B"))
+print(" A vale ", A," y B vale",B,)
+
 print("segundo ejercicio:")
-	print("la suma vale:", A + B,)
-	print("la resta vale:" A - B)
-	print("la multiplicación vale:" A * B)
-	print("la multiplicación vale:" A / B)
+print("la suma vale:", A + B,)
+print("la resta vale:", A - B,)
+print("la multiplicación vale:", A * B,)
+print("la multiplicación vale:", A / B,)
+print("tercer ejercicio")
+if A == B:
+	print("A y B son iguales")
+else:
+	if A > B :
+		print("A es mayor")
+	else:
+		print("B es mayor")
+print("ejercicio 4")
+print("introduce C")
+C = int(input("Escribir C"))
+print("comprobando el mayor")
+
+if A > B:
+	if C > A:
+		print("C es mayor")
+	else:
+		print("A es mayor")
+else:
+	if C > B:
+		print("C es mayor")
+	else:
+		print("B es mayor")
+print("quinto ejercicio")
+if A < 0:
+	print("A x B x C : ", A * B * C,)
+else:
+	print("A + B + C: ", A + B + C,)
+print("ejercicio 6 Raices y cuadrado")
+D = int(input("introduce D"))
+if D < 0:
+	print("error")
+else:
+	print("cuadrado de D",D**2,)
+	print("raiz de D: ", math.sqrt(D) ,)
+	

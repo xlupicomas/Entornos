1022e63ee6f163451a283b6910f126cabcfd605d
xlupicomas <xlupicomas@gmail.com>
2023-10-31 16:08:49 +0100

    final 15

--- main.py
+++ main.py
@@ -1,25 +1,77 @@
 import math
-import time
 
-print("insertar numeros")
-N = -1
-maximo = 0
-minimo = 9999999999999
-X = 0
-finbucle = False
-while finbucle == False:	
-    s = int(input(" "))
-    X = X + s
-    N = N + 1
-    if s > maximo:
-        maximo = s
-    if s < minimo and not s == 0:
-        minimo = s
-    if s == 0:
-        finbucle = True
+print("intorudce A")
+A = int(input("que numero es A"))
+print("introduce B")
+B = int(input("que numero es B"))
+print(" A vale ", A," y B vale",B,)
+
+print("segundo ejercicio:")
+print("intorudce A")
+A = int(input("que numero es A"))
+print("introduce B")
+B = int(input("que numero es B"))
+print("la suma vale:", A + B,)
+print("la resta vale:", A - B,)
+print("la multiplicación vale:", A * B,)
+print("la división vale:", A / B,)
+
+print("tercer ejercicio")
+print("intorudce A")
+A = int(input("que numero es A"))
+print("introduce B")
+B = int(input("que numero es B"))
+if A == B:
+    print("A y B son iguales")
+else:
+    if A > B :
+        print("A es mayor")
     else:
-        finnucle = False
-print("la media aritmetica es: ", X / N)
-print("maximo es: ", maximo)
-print("minimo es: ", minimo)
+        print("B es mayor")
 
+
+print("ejercicio 4")
+print("intorudce A")
+A = int(input("que numero es A"))
+print("introduce B")
+B = int(input("que numero es B"))
+print("introduce C")
+C = int(input("Escribir C"))
+print("comprobando el mayor")
+
+
+if A > B:
+    if C > A:
+        print("C es mayor")
+    else:
+        print("A es mayor")
+else:
+    if C > B:
+        print("C es mayor")
+    else:
+        print("B es mayor")
+
+
+print("quinto ejercicio")
+print("intorudce A")
+A = int(input("que numero es A"))
+print("introduce B")
+B = int(input("que numero es B"))
+print("intorudce C")
+C = int(input("que numero es C"))
+
+if A < 0:
+    print("A x B x C : ", A * B * C,)
+else:
+    print("A + B + C: ", A + B + C,)
+
+
+
+print("ejercicio 6 Raices y cuadrado")
+D = int(input("introduce D"))
+if D < 0:
+    print("error")
+else:
+    print("cuadrado de D",D**2,)
+    print("raiz de D: ", math.sqrt(D) ,)
+   

58203086cad520960bdcec77a0337293137978fe
Francisco Lupiañez Comas <xlupicomas@gmail.com>
2024-01-22 15:14:57 +0100

    to do list

--- HTML/actividad27/index.js
+++ HTML/actividad27/index.js
@@ -1,7 +1,7 @@
 //variable 1 para agarrar el input
 //variable 2 para agregar las listas
 let list = document.querySelector("#list");
-let orden = 1
+let orden = false
 function mas(inputbox) {
     addItem(inputbox);
     inputbox = "";
@@ -15,43 +15,102 @@ function addItem() {
     let todas = document.getElementById("todas")
     let hechas = document.getElementById("hechas")
     let puntero = document.createElement("i")
-    let copiadiv = div
+    let puntero2 = document.createElement("i")
     //puntero es la X
-    puntero.innerHTML = "X";
+    puntero2.innerHTML = "X";
+    puntero.innerHTML = "✔";
+    puntero.classList.add("separar")
 
     listItem.textContent = inputbox.value
     inputbox.value = ""
-    list.appendChild(div)
-    todas.appendChild(copiadiv)
     div.appendChild(listItem)
     div.appendChild(puntero)
-    //list.appendChild(listItem);
-    puntero.addEventListener("click", function(){
+    div.appendChild(puntero2)
+
+    let copiadiv = div.cloneNode(true)
+    copiadiv.classList.add("doneNone")
+    list.appendChild(div)
+    todas.appendChild(copiadiv)
+
+    puntero.addEventListener("click", function () {
         list.removeChild(div)
         hechas.appendChild(div)
+        listItem.classList.add("done")
+        console.log(copiadiv);
+        copiadiv.classList.remove("doneNone")
+        copiadiv.classList.add("doneAll")
+        //.classList.add("done")
     })
+    puntero2.addEventListener("click", function(){
+        list.removeChild(div)
+        todas.removeChild(copiadiv)
+        hechas.removeChild(div)
+        })
 }
+
 function ordenar() {
-    let Tipo = document.getElementById("tipo")
+    let elementos = document.getElementsByClassName("doneAll")
+    let elementosNone = document.getElementsByClassName("doneNone")
+    let todas = document.getElementById("todas");
+
+
+    if (orden == false) {
+        //codigo
+        let arrayElementos = Array.from(elementos)     //elementos.slice() //[].concat(elementos)
+        let n = 0
+        let x = 0
+        
+        for (n; n < arrayElementos.length; n++) {
+            console.log("aquí");
+            console.log(todas.childNodes[n].classList.contains("doneAll"))
+            if (todas.childNodes[n].classList.contains("doneAll")){
+                todas.removeChild(todas.childNodes[n])
+            }else{
+                console.log("no es doneAll");
+            }
+            
+        }
+
+        for (x; x < arrayElementos.length; x++) {
+            todas.appendChild(arrayElementos[x])
+        }
+        orden = true
+    }else{
+        let m = 0
+        let y = 0
+        let arrayElementosNone = Array.from(elementosNone)
+        for (m; m < arrayElementosNone.length; m++) {
+            if (todas.childNodes[m].classList.contains("doneNone")){
+                todas.removeChild(todas.childNodes[m])
+            }else{
+                console.log("no es doneNone");
+            }
+        }
+
+        for (y; y < arrayElementosNone.length; y++) {
+            todas.appendChild(arrayElementosNone[y])
+        }
+        orden = false
+    } 
+}
+function borrar() {
     let div = document.createElement("div");
     let listItem = document.createElement("li");
     let inputbox = document.getElementById("inputbox");
     let todas = document.getElementById("todas")
     let hechas = document.getElementById("hechas")
     let puntero = document.createElement("i")
-    if(orden == 3){
-        //codigo
-        orden = 1
-        alert("ordern = 3")
+    let puntero2 = document.createElement("i")
+    let m = 0
+    let k = 0
+    let l = 0
+    for (m; m < list.childNodes.length; m++) {
+        list.removeChild(list.childNodes[m])
     }
-    else if(orden == 2){
-        //codigo
-        orden = orden + 1
-        alert("ordern = 2")
+    for (l; l < hechas.childNodes.length; l++) {
+        hechas.removeChild(hechas.childNodes[l])
     }
-    else if (orden == 1){
-        //codigo
-        alert("ordern = 1")
-        orden = orden + 1
-    } 
+    for (k; k < todas.childNodes.length; k++) {
+        todas.removeChild(todas.childNodes[k])
+    }
 }

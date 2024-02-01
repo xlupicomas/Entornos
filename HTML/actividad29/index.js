425f43981cd02a8261ee8d6a998bcf2d737186f5
Francisco Lupiañez Comas <xlupicomas@gmail.com>
2024-02-01 20:37:11 +0100

    arreglando jq 2

--- HTML/actividad29/index.js
+++ HTML/actividad29/index.js
@@ -6,32 +6,7 @@ function rangeSlide2(value2) {
     document.getElementById('rangeValue2').innerHTML = value2;
 }
 
-function searchPrice(){
-$(document).ready(function () {
-    $("#Filtrar").click(function () {  
-        let precio = document.getElementsByClassName("precio")
-        let precioMin = document.getElementById('rangeValue').innerHTML;
-        let precioMax = document.getElementById('rangeValue2').innerHTML;
-        let n = 0
-        let carta = $(".carta")
-        console.log(precioMin)
-        console.log(precioMax)
-        for(n; n < carta.length; n++){
-            if(parseInt(precioMin) <= parseInt($(precio[n]).text())){
-                if(parseInt($(precio[n]).text()) <= parseInt(precioMax)){
-                    carta[n].classList.remove("displayNone")
-                }else{
-                    console.log("no dentro")
-                    carta[n].classList.add("displayNone")
-                }
-            }else{
-                console.log("no dentro")
-                carta[n].classList.add("displayNone")
-            }
-        }
-    });
-});
-}
+
 function searchPais(){
     $(document).ready(function () {
         let list = ["Cerdeña", "EEUU", "Mallorca", "Chipre", "Filipinas", "España", "Chile"];
@@ -58,4 +33,45 @@ function searchPais(){
     });
 });
 }
+function searchPrice(){
+    $(document).ready(function () {
+        $("#Filtrar").click(function () {  
+            let precio = document.getElementsByClassName("precio")
+            let precioMin = document.getElementById('rangeValue').innerHTML;
+            let precioMax = document.getElementById('rangeValue2').innerHTML;
+            let n = 0
+            let carta = $(".carta")
+            console.log(precioMin)
+            console.log(precioMax)
+            for(n; n < carta.length; n++){
+                if(parseInt(precioMin) <= parseInt($(precio[n]).text())){
+                    if(parseInt($(precio[n]).text()) <= parseInt(precioMax)){
+                        carta[n].classList.remove("displayNone2")
+                    }else{
+                        console.log("no dentro")
+                        carta[n].classList.add("displayNone2")
+                    }
+                }else{
+                    console.log("no dentro")
+                    carta[n].classList.add("displayNone2")
+                }
+            }
+        });
+    });
+    }
+    function searchDate() {
+        $(document).ready(function () {
+            $("#Filtrar").click(function () {
+                let dateInicio = document.getElementById("dateInicio")
+                let dateFinal = document.getElementById("dateFinal")
+                let dateInicioHTML = document.getElementsByClassName("dateInicio")
+                let dateFinalHTML = document.getElementsByClassName("dateFinal")
+                let n = 0
+                console.log($(dateInicio).val());
+                console.log($(dateFinal).val());
+                console.log($(dateInicioHTML[n]).text());
+                console.log($(dateFinalHTML[n]).text())
+            });
+        });
+    }
   
